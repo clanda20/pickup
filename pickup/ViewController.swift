35d076de4cedc2fe2b06c966da3e85f2103f0e37
christian landa <christian@christians-macbook-air.local>
2016-05-20 19:26:19 -0700

    Initial Commit

---
+++ pickup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pickup
+//
+//  Created by christian landa on 5/20/16.
+//  Copyright © 2016 christian landa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
